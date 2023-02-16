c2e01f34b2db0cc16947a524617a202027af6e2d
andreisophie <andreisophie@yahoo.com>
2023-02-16 16:45:57 +0200

    Added useState example

--- src/components/DescriptionCard/DescriptionCard.js
+++ src/components/DescriptionCard/DescriptionCard.js
@@ -1,13 +1,25 @@
 import "./DescriptionCard.css"
 import male from "../../assets/male.png"
 import female from "../../assets/female.png"
+import React, {useState} from "react"
+import { Button } from 'react-bootstrap'
 
 export default function DescriptionCard(props) {
+    const [style, setStyle] = useState("");
+
     const malePicture = <img src={male} className="gender-icon-m my-auto"/>
     const femalePicture = <img src={female} className="gender-icon-f my-auto"/>
 
+    function changeStyle() {
+        if (style === "") {
+            setStyle("favorite");
+        } else {
+            setStyle("");
+        }
+    }
+
     return (
-        <div className="card-container d-flex flex-column">
+        <div className={"card-container d-flex flex-column " + style}>
             <div className="d-flex flex-row align-items-start pb-3">
                 <img className="profile-pic" src = {props.profilePic}/>
                 <div className="d-flex flex-column p-3">
@@ -22,6 +34,9 @@ export default function DescriptionCard(props) {
             </div>
             <div className="other-elements">
                 {props.children}
+            </div>
+            <div>
+                <Button variant="outline-warning" onClick={changeStyle}>Favorite</Button>
             </div>
         </div>
     )
